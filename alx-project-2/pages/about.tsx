fb953d5ef0f8040c630ed1cc0545ec100f9f2e30
onyenike <onyenikefestus2019@gmail.com>
2025-12-19 13:27:00 -0800

    Delete alx-project-2 directory

--- alx-project-2/pages/about.tsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
- <h1 className="bg-red-800">
-    we build Houses
- </h1>
-      
-  );
-}
--- alx-project-2/pages/home.tsx
+++
@@ -1,6 +0,0 @@
-export default function home() {
-  return (
-  <>This is my home</>
-      
-  );
-}
